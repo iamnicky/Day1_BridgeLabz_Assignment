df7d35aac1faf2d86f2fc5c4a10069bf79518b78
iamnicky <2020ugee067@nitjsr.ac.in>
2023-11-20 11:44:24 +0530

    leap year code logic done

--- DetermineLeapYear.java
+++ DetermineLeapYear.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class DetermineLeapYear {
     public static void main(String[] args) {
-
+        System.out.println("Enter the year of your choice");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if(String.valueOf(year).length()==4){
+            if(year%4 == 0 && (year%400 == 0 || year%100 != 0)){
+                System.out.println(year + " is a leap Year");
+            }else {
+                System.out.println(year + " is a not a leap Year");
+            }
+        }else{
+            System.out.println("Invalid Input");
+        }
     }
 }
