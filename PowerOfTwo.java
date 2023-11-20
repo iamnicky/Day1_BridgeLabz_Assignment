b0d0554c55640843585efb63c0dd3b230f7ea9b6
iamnicky <2020ugee067@nitjsr.ac.in>
2023-11-20 12:02:32 +0530

    power of 2 code logic done

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class PowerOfTwo {
     public static void main(String[] args) {
-
+        System.out.println("Enter the power of N");
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int power=0 ; power < N ;power++){
+            long result = (long) Math.pow(2,power);
+            System.out.println(" Power of 2^" + power + " is: "  + result);
+        }
     }
 }
